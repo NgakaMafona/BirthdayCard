e040dddb9c0b96a9f1b9dc38d2fac62ef388fa4b
Ngaka <drmafona@gmail.com>
2016-10-11 09:32:00 +0200

    Birthday App Initial commit

---
+++ app/src/main/java/za/co/codetribe/praciceset1/MainActivity.java
@@ -0,0 +1,15 @@
+package za.co.codetribe.praciceset1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
